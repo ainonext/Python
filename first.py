c0ba6315b612cc545b8aaab59c8d466a89a6027f
StoryD <sm_sv_ma@mail.ru>
2014-06-12 20:30:45 +0400

    my first python

---
+++ first.py
@@ -0,0 +1,23 @@
+print("Hello, Py")
+strin= 5*'Hi '+ 'Python' + ' (='
+print(strin)
+
+x=0
+while x<=10:
+    print(x)
+    x+=1
+
+
+print("Word is Pythonchic")
+word = 'Pythonchic'
+a=word[:3]
+b=word[3:5]
+c=word[5:]
+
+print(a,b,c)
+print("D:")
+
+print("asdgadffcndgfdjsdfhsvbdbsdfhsfhjgbksajgdfadkfakdfkajsdf")
+text = 'asdgadffcndgfdjsdfhsvbdbsdfhsfhjgbksajgdfadkfakdfkajsdf'
+result=len(text)
+print("Lenght=",result)
